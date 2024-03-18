a931646b26bbb9108f0f98c615d772fdb4e8d86d
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-03-17 22:22:50 -0300

    Update emacs Config

---
+++ elpa/nasm-mode-1.1.1/nasm-mode-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from nasm-mode.el  -*- no-byte-compile: t -*-
+(define-package "nasm-mode" "1.1.1" "NASM x86 assembly major mode" '((emacs "24.3")) :authors '(("Christopher Wellons" . "wellons@nullprogram.com")) :maintainer '("Christopher Wellons" . "wellons@nullprogram.com") :url "https://github.com/skeeto/nasm-mode")
--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(tsdh-dark))
  '(inhibit-startup-screen t)
- '(package-selected-packages '(rust-mode lua-mode))
+ '(package-selected-packages '(nasm-mode rust-mode lua-mode))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
