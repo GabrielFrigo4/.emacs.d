4a5c47273645fbc2ba74e724b04ebf8578b280ae
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-03-22 14:19:12 -0300

    Update init.el

--- init.el
+++ init.el
@@ -6,10 +6,11 @@
  '(custom-enabled-themes '(tsdh-dark))
  '(inhibit-startup-screen t)
  '(package-selected-packages '(nasm-mode rust-mode lua-mode))
+ '(show-paren-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Noto Sans Mono" :foundry "GOOG" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Roboto Mono" :foundry "outline" :slant normal :weight regular :height 108 :width normal)))))
