2260a03f8c307058f9f610d48686e1a7626f8726
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-03-22 23:04:57 -0300

    Update emacs

--- init.el
+++ init.el
@@ -13,4 +13,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Roboto Mono" :foundry "outline" :slant normal :weight regular :height 108 :width normal)))))
+ '(default ((t (:family "RobotoMono Nerd Font Mono" :foundry "outline" :slant normal :weight regular :height 108 :width normal)))))
