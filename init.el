845eb23f601e3fd936789df972920db4df1791c3
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-03-30 17:37:51 -0300

    Update Emacs

--- init.el
+++ init.el
@@ -1,4 +1,12 @@
-;; EMACS Options
+;; Minimize garbage collection during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Lower threshold back to 8 MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (expt 2 23))))
+
+;; EMACS Auto-Options
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,55 +26,18 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "RobotoMono Nerd Font Mono" :foundry "outline" :slant normal :weight regular :height 108 :width normal)))))
 
-;; Remove Backup Files
-(setq make-backup-files nil)
-
-;; Setup Packages (MELPA)
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-resfresh-contents)
-  (package-install 'use-package))
-
-;; Install Packages
-(use-package try
-  :ensure t)
-(use-package nasm-mode
-  :ensure t)
-(use-package rust-mode
-  :ensure t)
-(use-package lua-mode
-  :ensure t)
-(use-package all-the-icons
-  :if (display-graphic-p))
-(use-package neotree
-  :ensure t
-  :config (setq neo-theme (if (display-graphic-p) 'arrow))
-  :bind ("C-\\" . 'neotree-toggle))
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
-(use-package which-key
-  :ensure t
-  :config
-  (progn
-    (which-key-mode)
-    (which-key-setup-side-window-right-bottom)
-    ))
-(use-package ace-window
-  :ensure t
-  :bind (("M-o" . ace-window)))
-
-;; Shortcut
-(global-set-key (kbd "C-<tab>") 'other-window)
-(global-set-key (kbd "C-M-<up>") 'enlarge-window)
-(global-set-key (kbd "C-M-<down>") 'shrink-window)
-(global-set-key (kbd "C-M-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "C-M-<left>") 'shrink-window-horizontally)
+(defconst dot-emacs (concat (getenv "HOME") "/.emacs.d/" "profile.el")
+    "My dot emacs file")
+ 
+(require 'bytecomp)
+(setq compiled-dot-emacs (byte-compile-dest-file dot-emacs))
+ 
+(if (or (not (file-exists-p compiled-dot-emacs))
+	(file-newer-than-file-p dot-emacs compiled-dot-emacs)
+        (equal (nth 4 (file-attributes dot-emacs)) (list 0 0)))
+    (load dot-emacs)
+  (load compiled-dot-emacs))
+ 
+(add-hook 'kill-emacs-hook
+          '(lambda () (and (file-newer-than-file-p dot-emacs compiled-dot-emacs)
+                           (byte-compile-file dot-emacs))))
---
+++ profile.el
@@ -0,0 +1,52 @@
+;; Remove Backup Files
+(setq make-backup-files nil)
+
+;; Setup Packages (MELPA)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-resfresh-contents)
+  (package-install 'use-package))
+
+;; Install Packages
+(use-package try
+  :ensure t)
+(use-package nasm-mode
+  :ensure t)
+(use-package rust-mode
+  :ensure t)
+(use-package lua-mode
+  :ensure t)
+(use-package all-the-icons
+  :if (display-graphic-p))
+(use-package neotree
+  :ensure t
+  :config (setq neo-theme (if (display-graphic-p) 'arrow))
+  :bind ("C-\\" . 'neotree-toggle))
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+(use-package which-key
+  :ensure t
+  :config
+  (progn
+    (which-key-mode)
+    (which-key-setup-side-window-right-bottom)
+    ))
+(use-package ace-window
+  :ensure t
+  :bind (("M-o" . ace-window)))
+
+;; Shortcut
+(global-set-key (kbd "C-<tab>") 'other-window)
+(global-set-key (kbd "C-M-<up>") 'enlarge-window)
+(global-set-key (kbd "C-M-<down>") 'shrink-window)
+(global-set-key (kbd "C-M-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-M-<left>") 'shrink-window-horizontally)
