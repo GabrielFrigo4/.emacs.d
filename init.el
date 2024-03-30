92b8f0f78082f307b5cbe7d0b9707fd41a561beb
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-03-30 15:07:35 -0300

    Update Emacs init.el

--- init.el
+++ init.el
@@ -1,24 +1,3 @@
-;; Remove Backup Files
-(setq make-backup-files nil)
-
-;; Setup Packages (MELPA)
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-resfresh-contents)
-  (package-install 'use-package))
-
-;; Install Packages
-(use-package try
-  :ensure t)
-(use-package )
-(use-package nasm-mode)
-(use-package rust-mode)
-(use-package lua-mode)
-
 ;; EMACS Options
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -29,7 +8,7 @@
  '(custom-enabled-themes '(tsdh-dark))
  '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
- '(package-selected-packages '(try))
+ '(package-selected-packages '(ergoemacs-mode ace-window try))
  '(show-paren-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -38,3 +17,56 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "RobotoMono Nerd Font Mono" :foundry "outline" :slant normal :weight regular :height 108 :width normal)))))
+
+;; Remove Backup Files
+(setq make-backup-files nil)
+
+;; Setup Packages (MELPA)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-resfresh-contents)
+  (package-install 'use-package))
+
+;; Install Packages
+(use-package try
+  :ensure t)
+(use-package nasm-mode
+  :ensure t)
+(use-package rust-mode
+  :ensure t)
+(use-package lua-mode
+  :ensure t)
+(use-package all-the-icons
+  :if (display-graphic-p))
+(use-package neotree
+  :ensure t
+  :config (setq neo-theme (if (display-graphic-p) 'arrow))
+  :bind ("C-\\" . 'neotree-toggle))
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+(use-package which-key
+  :ensure t
+  :config
+  (progn
+    (which-key-mode)
+    (which-key-setup-side-window-right-bottom)
+    ))
+(use-package ace-window
+  :ensure t
+  :bind (("M-o" . ace-window)))
+
+;; Shortcut
+(global-set-key (kbd "C-<tab>") 'other-window)
+(global-set-key (kbd "C-M-<up>") 'enlarge-window)
+(global-set-key (kbd "C-M-<down>") 'shrink-window)
+(global-set-key (kbd "C-M-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-M-<left>") 'shrink-window-horizontally)
