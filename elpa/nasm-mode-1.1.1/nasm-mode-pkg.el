51ce1375ca902b093db5b74c2c62ad4bd9e5cf9c
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-03-30 11:48:33 -0300

    Update emacs

--- elpa/nasm-mode-1.1.1/nasm-mode-pkg.el
+++ elpa/nasm-mode-1.1.1/nasm-mode-pkg.el
@@ -1,2 +1,12 @@
-;; Generated package description from nasm-mode.el  -*- no-byte-compile: t -*-
-(define-package "nasm-mode" "1.1.1" "NASM x86 assembly major mode" '((emacs "24.3")) :authors '(("Christopher Wellons" . "wellons@nullprogram.com")) :maintainer '("Christopher Wellons" . "wellons@nullprogram.com") :url "https://github.com/skeeto/nasm-mode")
+(define-package "nasm-mode" "1.1.1" "NASM x86 assembly major mode"
+  '((emacs "24.3"))
+  :commit "d990ed94d902b74a5c834fb567e03307607cee45" :authors
+  '(("Christopher Wellons" . "wellons@nullprogram.com"))
+  :maintainers
+  '(("Christopher Wellons" . "wellons@nullprogram.com"))
+  :maintainer
+  '("Christopher Wellons" . "wellons@nullprogram.com")
+  :url "https://github.com/skeeto/nasm-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- init.el
+++ init.el
@@ -1,11 +1,35 @@
+;; Remove Backup Files
+(setq make-backup-files nil)
+
+;; Setup Packages (MELPA)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-resfresh-contents)
+  (package-install 'use-package))
+
+;; Install Packages
+(use-package try
+  :ensure t)
+(use-package )
+(use-package nasm-mode)
+(use-package rust-mode)
+(use-package lua-mode)
+
+;; EMACS Options
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(custom-enabled-themes '(tsdh-dark))
+ '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
- '(package-selected-packages '(nasm-mode rust-mode lua-mode))
+ '(package-selected-packages '(try))
  '(show-paren-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -14,4 +38,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "RobotoMono Nerd Font Mono" :foundry "outline" :slant normal :weight regular :height 108 :width normal)))))
-(setq make-backup-files nil)
