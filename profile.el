70af58fadbb9649ff304058d6d815e7e4c629297
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-04-07 15:27:35 -0300

    Update profile.el

--- profile.el
+++ profile.el
@@ -69,11 +69,12 @@
 
 ;; Command
 (global-set-key (kbd "<apps>") 'execute-extended-command)
+(global-set-key (kbd "<menu>") 'execute-extended-command)
 
 ;; Function Keys (not used by default)
-(global-set-key (kbd "<f5>") ctl-x-map)
-(global-set-key (kbd "<f6>") ctl-x-map)
-(global-set-key (kbd "<f7>") ctl-x-map)
-(global-set-key (kbd "<f8>") ctl-x-map)
+(global-set-key (kbd "<f5>") 'find-file)
+(global-set-key (kbd "<f6>") 'dired)
+(global-set-key (kbd "<f7>") (kbd ""))
+(global-set-key (kbd "<f8>") (kbd ""))
 (global-set-key (kbd "<f9>") ctl-x-map)
-(global-set-key (kbd "<f12>") ctl-x-map)
+(global-set-key (kbd "<f12>") (kbd ""))
