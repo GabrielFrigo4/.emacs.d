19d6a7f4e70f3bc27b2f7cf1ca1bd786e34c0d47
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-04-11 20:42:12 -0300

    Update sartup.el

--- Emacs-Lisp/sartup.el
+++ Emacs-Lisp/sartup.el
@@ -2,9 +2,9 @@
 ;; Startup
 ;;################
 
+;; Set to 'fundamental-mode
+(setq initial-major-mode 'fundamental-mode)
+
 ;; Remove message
 (setq inhibit-startup-message 't)
 (setq initial-scratch-message 'nil)
-
-;; Set to 'fundamental-mode
-(setq initial-major-mode 'fundamental-mode)
