1af6b829d618524e978456c95ec0d360ba84e8f2
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-04-16 16:41:56 -0300

    Update settings.el

--- Emacs-Lisp/settings.el
+++ Emacs-Lisp/settings.el
@@ -4,3 +4,17 @@
 
 ;; Remove backup files
 (setq make-backup-files nil)
+
+;; Set default tab-wight
+(setq-default tab-width 4)
+
+;; Set tab on cc-mode
+(setq-default indent-tabs-mode t)
+(defvaralias 'c-basic-offset 'tab-width)
+
+;; Set tab on ruby-mode
+(setq ruby-indent-tabs-mode t)
+(defvaralias 'ruby-indent-level 'tab-width)
+
+;; Set tab on sgml-mode
+(defvaralias 'sgml-basic-offset 'tab-width)
