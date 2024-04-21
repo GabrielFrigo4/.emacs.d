83a785323a206cd9e57964425edfc5ad66256edc
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-04-21 18:00:36 -0300

    Update package.el

--- Emacs-Lisp/package.el
+++ Emacs-Lisp/package.el
@@ -11,10 +11,6 @@
   :ensure t)
 (use-package lua-mode
   :ensure t)
-(use-package neotree
-  :ensure t
-  :config (setq neo-theme (if (display-graphic-p) 'arrow))
-  :bind ("C-\\" . 'neotree-toggle))
 (use-package auto-complete
   :ensure t
   :init
@@ -32,6 +28,19 @@
 (use-package ace-window
   :ensure t
   :bind (("M-o" . ace-window)))
+(when (eq system-type 'gnu/linux)
+
+)
+(if (eq system-type 'windows-nt)
+  (use-package neotree
+    :ensure t
+    :config (setq neo-theme (if (display-graphic-p) 'arrow))
+    :bind ("C-\\" . 'neotree-toggle))
+  (use-package neotree
+    :ensure t
+    :config (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+    :bind ("C-\\" . 'neotree-toggle))
+)
 
 ;; Settings Packages
 (setq-default neo-show-hidden-files t)
