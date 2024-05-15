c68e6609052c7ddd7a8b30b10a0e3435cab974e5
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-05-15 17:18:44 -0300

    Update settings-init.el

--- Emacs-Lisp/settings-init.el
+++ Emacs-Lisp/settings-init.el
@@ -4,19 +4,18 @@
 
 
 ;; Remove backup files
-(setq make-backup-files nil)
+(setq-default make-backup-files nil)
 
 ;; Set default tab-wight
 (setq-default tab-width 4)
 
-;; Set tab default
-(setq-default indent-tabs-mode t)
-(setq-default ruby-indent-tabs-mode t)
-
 ;; Set tab on cc-mode
-(setq-default c-basic-offset tab-width)
+(setq-default indent-tabs-mode t)
+(setq-default c-ts-mode-indent-offset 4)
+(setq-default c-basic-offset 4)
 
 ;; Set tab on ruby-mode
+(setq-default ruby-indent-tabs-mode t)
 (setq-default ruby-indent-level tab-width)
 
 ;; Set tab on sgml-mode
@@ -26,6 +25,6 @@
 (setq-default sh-basic-offset tab-width)
 
 ;; Set tab on python-mode
-(setq-default python-indent-guess-indent-offset t)  
+(setq-default python-indent-guess-indent-offset t)
 (setq-default python-indent-guess-indent-offset-verbose nil)
 (setq-default python-indent-offset tab-width)
