488ce970466685360940433dbf58fba0e7ff43b3
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-06-04 23:14:51 -0300

    Fix Python Tab

--- #Emacs-Lisp/Custom/Python Mode.el
+++ #Emacs-Lisp/Custom/Python Mode.el
@@ -62,6 +62,7 @@
   (setq indent-tabs-mode t)
   (setq python-indent 8)
   (setq tab-width 4)
+  (setq python-indent-offset tab-width)
   )
 
 (add-hook 'python-mode-hook #'custom-python-mode-hook)
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This repository is the configuration of the Emacs code editor that Gabriel Frigo
 ## TODO
 Melhorar
 ```
-Atualizar Tab Python
+Atualizar Python
 Atualizar Lua
 Atualizar C/C++
 ```
