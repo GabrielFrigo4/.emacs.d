02fcdad20017b6765bba2b100f5b8ab5411c7d76
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-06-15 22:52:53 -0300

    Update C and C++ Mode.el

--- #Emacs-Lisp/Custom/C and C++ Mode.el
+++ #Emacs-Lisp/Custom/C and C++ Mode.el
@@ -8,7 +8,8 @@
 		          ("\\<\\(STDIN_FILENO\\|STDOUT_FILENO\\|STDERR_FILENO\\)\\>" . font-lock-constant-face)
 		          ("\\<\\(int8_t\\|int16_t\\|int32_t\\|int64_t\\)\\>" . font-lock-type-face)
 		          ("\\<\\(uint8_t\\|uint16_t\\|uint32_t\\|uint64_t\\)\\>" . font-lock-type-face)
-              ("\\<\\(printf\\|scanf\\|fopen\\)\\>" . font-lock-builtin-face)
+              ("\\<\\(printf\\|scanf\\|fopen\\|malloc\\|free\\)\\>" . font-lock-builtin-face)
+              ("\\<\\(cin\\|wcin\\|cout\\|wcout\\|cerr\\|wcerr\\)\\>" . font-lock-builtin-face)
               ))
 
 (font-lock-add-keywords
