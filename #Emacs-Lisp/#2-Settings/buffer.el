9dc6377182eabd54de9d4db4070f2109a925daa6
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-12-27 12:33:29 -0300

    Update buffer.el

--- #Emacs-Lisp/#2-Settings/buffer.el
+++ #Emacs-Lisp/#2-Settings/buffer.el
@@ -3,6 +3,9 @@
 ;; ################
 
 
+;; Def *init-buffers*
+(setq init-buffers '("*Warnings*" "*quelpa-build-checkout*" "*scratch*"))
+
 ;; Def *kill-all-buffers*
 (defun kill-all-buffers ()
   "Kill all Buffers."
@@ -14,3 +17,13 @@
   "Kill all other Buffers."
   (interactive)
   (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+
+;; Def *kill-init-buffers*
+(defun kill-init-buffers ()
+  "Kill Initial Buffers after Initialization."
+  (dolist (buf init-buffers)
+    (when (get-buffer buf)
+      (kill-buffer buf))))
+
+;; Kill Initial Buffers
+(add-hook 'after-init-hook #'kill-init-buffers)
