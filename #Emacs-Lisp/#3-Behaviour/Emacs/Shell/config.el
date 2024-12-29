dc9242a1284143bd895036c556ded7c5253a5894
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-12-29 13:53:17 -0300

    Update config.el

--- #Emacs-Lisp/#3-Behaviour/Emacs/Shell/config.el
+++ #Emacs-Lisp/#3-Behaviour/Emacs/Shell/config.el
@@ -3,16 +3,7 @@
 ;; ################
 
 
-(defun config-shell-mode-hook ()
-  ;; Disable font-locking in this buffer to improve performance
-  (font-lock-mode -1)
-  ;; Prevent font-locking from being re-enabled in this buffer
-  (make-local-variable 'font-lock-function)
-  (setq font-lock-function (lambda (_) nil))
-  (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)
-  )
-
-(setq comint-output-filter-functions
-      (remove 'ansi-color-process-output comint-output-filter-functions))
-
-(add-hook 'shell-mode-hook #'config-shell-mode-hook)
+(if-windows
+  (setq shell-file-name "cmd"))
+(if-linux
+  (setq shell-file-name "zsh"))
