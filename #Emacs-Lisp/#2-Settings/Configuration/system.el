1c7c7163df320cd00bd9e7c58bae9eed1b520139
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-01-05 17:40:46 -0300

    Update system.el

--- #Emacs-Lisp/#2-Settings/Configuration/system.el
+++ #Emacs-Lisp/#2-Settings/Configuration/system.el
@@ -12,11 +12,30 @@
 ;; Remove backup files
 (setq-default make-backup-files nil)
 
-;; Set default to UTF-8
-(set-default-coding-systems 'utf-8)
-
 ;; On OSX, swap Meta and Super
 ;; For Better Keyboard Ergonomics
 (if-darwin
  (setq-default mac-command-modifier 'meta)
  (setq-default mac-option-modifier 'super))
+
+
+;; ################
+;; # Coding
+;; ################
+
+
+;; Import *ob-async*
+(require 'ob-async)
+
+;; Def *set-coding-systems*
+(setq-default setup-coding-systems
+              (lambda ()
+                (set-default-coding-systems 'utf-8)
+                (setq-default coding-system-for-read 'utf-8)
+                (setq-default coding-system-for-write 'utf-8)))
+
+;; Setup Coding Systems
+(funcall setup-coding-systems)
+
+;; Add Hook *ob-async-pre-execute-src-block-hook*
+(add-hook 'ob-async-pre-execute-src-block-hook setup-coding-systems)
