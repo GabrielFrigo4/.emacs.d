6618f2f27c751d43517f205d0036f7cc92764078
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-01-28 12:51:34 -0300

    Update Packages

--- #Emacs-Lisp/#1-Packages/QUELPA.el
+++ #Emacs-Lisp/#1-Packages/QUELPA.el
@@ -27,3 +27,11 @@
 ;; Install Aweshell
 (use-package aweshell
   :quelpa (aweshell :fetcher github :repo "GabrielFrigo4/aweshell"))
+
+;; Install FASM
+(use-package fasm-mode
+  :quelpa (fasm-mode :fetcher github :repo "GabrielFrigo4/fasm-mode"))
+
+;; Install NASM
+(use-package nasm-mode
+  :quelpa (nasm-mode :fetcher github :repo "GabrielFrigo4/nasm-mode"))
