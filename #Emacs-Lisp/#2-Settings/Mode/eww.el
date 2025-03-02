5d46384b32f557c7cba985b531453f84e82abc38
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-03-02 00:33:43 -0300

    Update eww.el

--- #Emacs-Lisp/#2-Settings/Mode/eww.el
+++ #Emacs-Lisp/#2-Settings/Mode/eww.el
@@ -3,8 +3,8 @@
 ;; ################
 
 
-;; Unix Manual Web
-(defun unix-man (command)
+;; Unix Manual Online
+(defun mandoc (command)
   "Unix Manual Pages Online (NetBSD Manual Pages)"
   (interactive "sUnix Manual Pages Online: ")
   (let ((url (format "https://man.netbsd.org/%s" (url-hexify-string command))))
