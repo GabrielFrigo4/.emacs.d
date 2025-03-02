ebca128a7969a8e8f9846707b5c440f8b4f07029
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-03-02 14:51:43 -0300

    Update eww.el

--- #Emacs-Lisp/#2-Settings/Mode/eww.el
+++ #Emacs-Lisp/#2-Settings/Mode/eww.el
@@ -4,8 +4,10 @@
 
 
 ;; Unix Manual Online
-(defun mandoc (command)
-  "Unix Manual Pages Online (NetBSD Manual Pages)"
-  (interactive "sUnix Manual Pages Online: ")
-  (let ((url (format "https://man.netbsd.org/%s" (url-hexify-string command))))
+(defun mandoc (section command)
+  "Unix Manual Pages Online (Linux Manual Pages).
+SECTION is the manual section (e.g., 1 for general commands).
+COMMAND is the name of the command to look up."
+  (interactive "sManual Section: \nsCommand: ")
+  (let ((url (format "https://man.cx/%s(%s)" (url-hexify-string command) (url-hexify-string section))))
     (eww url)))
