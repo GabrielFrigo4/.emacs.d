cc348c03e15897795378b5a30b98216ed1b55114
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-03-02 13:11:45 -0300

    Update EAF.el

--- #Emacs-Lisp/#1-Packages/EAF.el
+++ #Emacs-Lisp/#1-Packages/EAF.el
@@ -6,16 +6,14 @@
 ;; Load EAF Path
 (add-to-list 'load-path (concat (getenv "HOME") "/.emacs.d/eaf/emacs-application-framework/"))
 
-;; Setup EAF
+;; Setup EAF on Linux
 (when-linux
- (require 'eaf))
-
-;; Setup Apps on Linux
-(when-linux
+ ;; Setup EAF
+ (require 'eaf)
+ ;; Setup Apps
  (require 'eaf-browser)
- (require 'eaf-pdf-viewer))
-
-;; Remove EAF Buffers
-(dolist (buf (list "*eaf*" "*eaf-epc con 4*"))
-  (when (get-buffer buf)
-    (kill-buffer buf)))
+ (require 'eaf-pdf-viewer)
+ ;; Kill Buffers
+ (dolist (buf (list "*eaf*" "*eaf-epc con 4*"))
+   (when (get-buffer buf)
+     (kill-buffer buf))))
