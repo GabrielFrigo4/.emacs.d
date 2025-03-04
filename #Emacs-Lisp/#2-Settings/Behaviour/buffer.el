4d953b6b103c41cead0529da4669814f0987e605
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-03-04 01:53:59 -0300

    Update buffer.el

--- #Emacs-Lisp/#2-Settings/Behaviour/buffer.el
+++ #Emacs-Lisp/#2-Settings/Behaviour/buffer.el
@@ -4,7 +4,8 @@
 
 
 ;; Def *init-buffers*
-(setq-default init-buffers '("*Warnings*" "*Messages*" "*Shell Configuration*" "*Async-native-compile-log*" "*quelpa-build-checkout*" "*straight-process*"))
+(setq-default init-buffers '("*Warnings*" "*Messages*" "*Shell Configuration*" "*quelpa-build-checkout*" "*straight-process*"))
+(setq-default org-buffers '("*Async-native-compile-log*"))
 
 ;; Def *kill-all-buffers*
 (defun kill-all-buffers ()
@@ -26,6 +27,14 @@
     (when (get-buffer buf)
       (kill-buffer buf))))
 
+;; Def *kill-org-buffers*
+(defun kill-org-buffers ()
+  "Kill Org Mode Buffers after Setup."
+  (interactive)
+  (dolist (buf org-buffers)
+    (when (get-buffer buf)
+      (kill-buffer buf))))
+
 ;; Def *setup-buffer-eol*
 (defun setup-buffer-eol ()
   "Do not show ^M in files containing mixed UNIX and DOS line endings."
@@ -41,11 +50,11 @@
 
 
 ;; Kill Initial Buffers
-(add-hook 'org-load-hook #'kill-init-buffers)
 (add-hook 'emacs-startup-hook #'kill-init-buffers)
-(add-hook 'window-setup-hook #'kill-init-buffers)
-(add-hook 'eaf-after-init-hook #'kill-init-buffers)
 (add-hook 'elpaca-after-init-hook #'kill-init-buffers)
+
+;; Kill Org Mode Buffers
+(add-hook 'org-load-hook #'kill-org-buffers)
 
 ;; Setup Buffer EOL
 (add-hook 'after-init-hook #'setup-buffer-eol)
