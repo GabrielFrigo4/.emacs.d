307576bf29baa95d952e95aef7b726b1b0a14928
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2025-04-29 15:36:10 -0300

    Update EWW

--- #Emacs-Lisp/#2-Settings/Mode/eww.el
+++ #Emacs-Lisp/#2-Settings/Mode/eww.el
@@ -6,13 +6,6 @@
 ;; Import EWW
 (require 'eww)
 
-;; Setup "Emacs Web Wowser"
-(setq-default browse-url-browser-function 'browse-url-default-browser)
-
-;; Enable Better HTML/CSS/JS Result
-(setq-default url-queue-timeout (expt 2 4))
-(setq-default url-user-agent 'default)
-
 
 ;; ################
 ;; # SHR
@@ -22,12 +15,6 @@
 ;; Import SHR
 (require 'shr)
 (require 'shr-tag-pre-highlight)
-
-;; Setup "Simple HTML Reader" (SHR)
-(setq-default shr-nbsp ?\u00A0)
-(setq-default shr-inhibit-images nil)
-(setq-default shr-use-fonts nil)
-(setq-default shr-width (+ (expt 2 6) (expt 2 4)))
 
 
 ;; ################
