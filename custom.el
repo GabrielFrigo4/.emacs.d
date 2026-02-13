a59a1c188773acba23495b0b91536830d67d13bc
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2026-02-13 13:50:09 -0300

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,21 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode 1)
+ '(column-number-mode t)
  '(custom-enabled-themes '(doom-dark+))
  '(custom-safe-themes
    '("f33b5dfb5c5fb99b5a90feab9158cadc2588c6840211b995622a35419c450b04"
 	   "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
 	   default))
- '(display-battery-mode 1)
+ '(display-battery-mode t)
  '(display-line-numbers-type 'relative)
- '(display-time-mode 1)
- '(global-display-line-numbers-mode 1)
+ '(display-time-mode t)
+ '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
- '(size-indication-mode 1)
+ '(size-indication-mode t)
  '(tool-bar-mode nil)
- '(xterm-mouse-mode 1))
+ '(xterm-mouse-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
