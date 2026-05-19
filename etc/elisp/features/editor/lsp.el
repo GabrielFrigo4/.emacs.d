eb232e6bc3bd43ac6e383635d55a4a69a7b8e82c
Gabriel Frigo <gabriel.frigo4@gmail.com>
2026-05-19 00:24:47 -0300

    Update Emacs

--- etc/elisp/features/editor/lsp.el
+++ etc/elisp/features/editor/lsp.el
@@ -2,6 +2,11 @@
 ;;  LSP FEATURE
 ;; ============================================================================
 
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode 1))
+
 (use-package lsp-mode
   :ensure (:type git :host github :repo "emacs-lsp/lsp-mode" :branch "master")
   :hook ((c-mode . lsp-deferred)
@@ -21,7 +26,14 @@
   :config
   (add-to-list 'lsp-disabled-clients 'semgrep-ls)
   (with-eval-after-load 'flymake
-    (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)))
+    (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake))
+
+  (defun lsp-go-install-save-hooks ()
+    (setq-local apheleia-inhibit t)
+    (add-hook 'before-save-hook #'lsp-organize-imports nil t)
+    (add-hook 'before-save-hook #'lsp-format-buffer nil t))
+  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+  (add-hook 'go-ts-mode-hook #'lsp-go-install-save-hooks))
 
 (use-package lsp-ui
   :ensure (:type git :host github :repo "emacs-lsp/lsp-ui" :branch "master")
