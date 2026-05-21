540c04bd1e8fa414e557a3fcd9f62619e2935a0c
Gabriel Frigo <gabriel.frigo4@gmail.com>
2026-05-21 16:49:44 -0300

    Update Sintaxe

--- etc/tools/tools.el
+++ etc/tools/tools.el
@@ -8,7 +8,6 @@
 (use-package elenv :ensure (:host github :repo "jcs-elpa/elenv"))
 (use-package msgu :ensure (:host github :repo "jcs-elpa/msgu"))
 (use-package dash :ensure t :config (global-dash-fontify-mode))
-(use-package auto-scroll-bar :hook (prog-mode . auto-scroll-bar-mode) :ensure (:host github :repo "emacs-vs/auto-scroll-bar"))
 (use-package mixed-pitch :ensure t)
 (use-package goto-chg :ensure t :bind ("C-." . goto-last-change))
 
